eef6d6e4d847f5676db441c4a80c0931950091f2
Yaroslav <6715414@gmail.com>
2023-10-22 18:48:13 +0300

    Updated command names

--- 03_cli_telegram-console/app.js
+++ 03_cli_telegram-console/app.js
@@ -6,19 +6,21 @@ program.version("1.0.0").description("CLI: TELEGRAM CONSOLE SENDER");
 
 const commands = [
   {
-    cmd: "send-message <message>",
+    cmd: "message <message>",
+    alias: "m",
     description: "Send a message to the Telegram bot",
     action: sendMessage,
   },
   {
-    cmd: "send-photo <path>",
+    cmd: "photo <path>",
+    alias: "p",
     description: "Send a photo to the Telegram bot",
     action: sendPhoto,
   },
 ];
 
-commands.forEach(({ cmd, description, action }) => {
-  program.command(cmd).description(description).action(action);
+commands.forEach(({ cmd, description, alias, action }) => {
+  program.command(cmd).alias(alias).description(description).action(action);
 });
 
 program.parse(process.argv);
