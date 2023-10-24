6151ddeedd07e899705ab23f22efbb878078b5db
Yaroslav <6715414@gmail.com>
2023-10-24 12:29:02 +0300

    Added constants

---
+++ 04_cli_telegram_weather/consts.js
@@ -0,0 +1,19 @@
+// Load Env
+require("./libs/loadEnvFromFile");
+
+const botToken = process.env.BOT_TOKEN;
+const weatherToken = process.env.API_WEATHER_KEY;
+
+const city = "Kharkiv";
+const intervalEnum = {
+  3: 1,
+  6: 2,
+};
+const COMMANDS = {
+  START: /\/start/,
+  FORECAST: `Forecast in ${city}`,
+  INTERVAL_3: "At intervals of 3 hours",
+  INTERVAL_6: "At intervals of 6 hours",
+};
+
+module.exports = { botToken, weatherToken, city, intervalEnum, COMMANDS };
