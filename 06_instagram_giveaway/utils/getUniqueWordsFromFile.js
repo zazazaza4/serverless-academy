ad06ffb855c415e5a4270f5f8f03000db50b414a
Yaroslav <6715414@gmail.com>
2023-10-25 12:30:55 +0300

    Improved getUniqueWordsFromFile function

--- 06_instagram_giveaway/utils/getUniqueWordsFromFile.js
+++ 06_instagram_giveaway/utils/getUniqueWordsFromFile.js
@@ -3,8 +3,13 @@ const fs = require("fs").promises;
 const getUniqueWordsFromFile = async (filePath) => {
   try {
     const fileContent = await fs.readFile(filePath, "utf-8");
-    const words = fileContent.split("\n");
-    const uniqueWords = new Set(words);
+    const lines = fileContent.split("\n");
+    const uniqueWords = new Set();
+
+    for (const word of lines) {
+      uniqueWords.add(word);
+    }
+
     return Array.from(uniqueWords);
   } catch (err) {
     console.error(`Error reading file '${filePath}': ${err.message}`);
