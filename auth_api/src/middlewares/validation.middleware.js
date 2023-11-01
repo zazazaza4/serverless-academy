671518fd919b0f1d4534e6a654f2c4e0ad35e95c
Yaroslav <6715414@gmail.com>
2023-11-01 13:29:39 +0200

    Added validation

---
+++ auth_api/src/middlewares/validation.middleware.js
@@ -0,0 +1,25 @@
+const serverResponse = require('../utils/responses');
+const { BAD_REQUEST } = require('../constants/messages');
+const Validator = require('../utils/validator');
+
+const validationUser = (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!Validator.isEmail(email)) {
+      throw new Error('Invalid email format');
+    }
+
+    if (!Validator.lengthRange(password, 6, 20)) {
+      throw new Error('Password must be between 6 and 20 characters');
+    }
+
+    next();
+  } catch (error) {
+    serverResponse.sendError(res, { ...BAD_REQUEST, error: error.message });
+  }
+};
+
+module.exports = {
+  validationUser,
+};
--- auth_api/src/routes/api/auth.js
+++ auth_api/src/routes/api/auth.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 const authController = require('../../controllers/auth.controller');
+const validation = require('../../middlewares/validation.middleware');
 
-router.post('/sign-in', authController.signIn);
-router.post('/sign-up', authController.signUp);
+router.post('/sign-in', validation.validationUser, authController.signIn);
+router.post('/sign-up', validation.validationUser, authController.signUp);
 
 module.exports = router;
