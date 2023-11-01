27470cdd1befc29b2fb6bf3f436b27caf31b5767
Yaroslav <6715414@gmail.com>
2023-11-01 11:49:13 +0200

    Added auth middleware

---
+++ auth_api/src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+const serverResponse = require('../utils/responses');
+const userTokenService = require('../services/userToken.service');
+const {
+  UNAUTHORIZED,
+  AUTHENTICATION_FAILED,
+} = require('../constants/messages');
+
+const auth = async (req, res, next) => {
+  try {
+    const [bearer, token] = req?.headers?.authorization?.split(' ');
+
+    if (bearer !== 'Bearer') {
+      serverResponse.sendError(res, UNAUTHORIZED);
+      return;
+    }
+
+    const user = userTokenService.verifyToken(token);
+
+    req.user = user;
+    next();
+  } catch (error) {
+    serverResponse.sendError(res, AUTHENTICATION_FAILED);
+  }
+};
+
+module.exports = auth;
