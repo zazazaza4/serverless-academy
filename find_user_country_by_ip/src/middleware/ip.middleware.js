d23bc366ad04f81277eed324224b03009e03444f
Yaroslav <6715414@gmail.com>
2023-11-02 11:49:31 +0200

    Added headers

--- find_user_country_by_ip/src/middleware/ip.middleware.js
+++ find_user_country_by_ip/src/middleware/ip.middleware.js
@@ -1,7 +1,12 @@
 const ipToInt = require('../utils/ipToInt');
 
 const getIP = (req, res, next) => {
-  const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const userIp =
+    request.headers['cf-connecting-ip'] ||
+    request.headers['x-real-ip'] ||
+    request.headers['x-forwarded-for'] ||
+    request.socket.remoteAddress ||
+    '';
 
   req.ip = userIp;
   req.userIP = ipToInt(userIp);
