099d38fe5441414c1b3a4f5799b3c87669fab4e8
Yaroslav <6715414@gmail.com>
2023-11-03 14:46:38 +0200

    Added json routes

--- json_storage/src/routes/api/index.js
+++ json_storage/src/routes/api/index.js
@@ -1,3 +1,7 @@
 const router = require('express').Router();
 
+const jsonRoutes = require('./json');
+
+router.use('/location', jsonRoutes);
+
 module.exports = router;
---
+++ json_storage/src/routes/api/json.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/:json_path', () => {});
+
+router.put('/:json_path', () => {});
+
+module.exports = router;
