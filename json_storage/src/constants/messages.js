d27848b7cc9049ac2154f8b446f93b45605a1721
Yaroslav <6715414@gmail.com>
2023-11-03 14:45:54 +0200

    Added messages

---
+++ json_storage/src/constants/messages.js
@@ -0,0 +1,20 @@
+module.exports = {
+  INTERNAL_SERVER_ERROR: {
+    code: 500,
+    success: false,
+    error: 'Something unexpected happened',
+  },
+  BAD_REQUEST: {
+    code: 400,
+    error: 'Bad request. Please try again with valid parameters',
+    success: false,
+  },
+  SUCCESSFUL: {
+    code: 200,
+    success: true,
+  },
+  SUCCESSFUL_CREATED: {
+    code: 201,
+    success: true,
+  },
+};
