e60ec9bdfdeb9a3c72aa7773de7c6d035629edc1
Yaroslav <6715414@gmail.com>
2023-11-04 14:45:36 +0200

    Changed Int id to UUID

--- auth_api/src/migrations/tables.sql
+++ auth_api/src/migrations/tables.sql
@@ -1,6 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE
     users (
-        id SERIAL PRIMARY KEY,
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
         email VARCHAR(255) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL
     );
@@ -8,7 +10,7 @@ CREATE TABLE
 CREATE TABLE
     usersToken (
         token_id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id),
+        user_id UUID REFERENCES users(id),
         token VARCHAR(255) NOT NULL,
         created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
     );
