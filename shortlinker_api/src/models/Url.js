bb55d2070a0057d32df7250a44063ef0d334846c
Yaroslav <6715414@gmail.com>
2023-11-05 17:55:20 +0200

    Added Url model

---
+++ shortlinker_api/src/models/Url.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UrlSchema = new mongoose.Schema({
+  origUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  date: {
+    type: String,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Url', UrlSchema);
