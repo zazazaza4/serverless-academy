227b204d6058e90f6398ec241f73f13e7d87d94b
Yaroslav <6715414@gmail.com>
2023-11-09 14:32:00 +0200

    Improved error handling and readability in getMe method

--- auth_api/src/controllers/user.controller.js
+++ auth_api/src/controllers/user.controller.js
@@ -1,16 +1,25 @@
 const userService = require('../services/user.service');
 const serverResponse = require('../utils/responses');
-const { INTERNAL_SERVER_ERROR, SUCCESSFUL } = require('../constants/messages');
+const {
+  INTERNAL_SERVER_ERROR,
+  SUCCESSFUL,
+  NOT_FOUND,
+} = require('../constants/messages');
 const logger = require('../utils/logger');
 
 class UserController {
   async getMe(req, res) {
     try {
       const { id } = req.user;
+      const userData = await userService.getMe(id);
 
-      const { email } = await userService.getMe(id);
+      if (!userData) {
+        return serverResponse.sendError(res, NOT_FOUND);
+      }
 
-      serverResponse.sendSuccess(res, { id, email }, SUCCESSFUL);
+      const userResponse = { id, email };
+
+      serverResponse.sendSuccess(res, userResponse, SUCCESSFUL);
     } catch (error) {
       logger.error(error.message);
       serverResponse.sendError(res, INTERNAL_SERVER_ERROR);
